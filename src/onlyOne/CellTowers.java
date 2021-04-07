90659558a3ea7f782a619e5ea9b6be37451b389c
Chase Dawson <chase_dawson7@mymail.eku.edu>
2021-04-07 13:35:16 -0400

    - Implement greedy algorithm (has errors) - Add test run for greedy algorithm

--- src/onlyOne/CellTowers.java
+++ src/onlyOne/CellTowers.java
@@ -1,7 +1,8 @@
 package onlyOne;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Hashtable;
+import java.util.Map.Entry;
 
 public class CellTowers {
 
@@ -15,10 +16,56 @@ public class CellTowers {
 		for (Integer x : bfResult) {
 			System.out.print(x + " ");
 		}
+		
+		// Test greedy algorithm
+		ArrayList<Integer> greedyResult = greedyTowerPlacement(testHouses);
+		System.out.println("Number of cell towers Required: " + greedyResult.size());
+		System.out.print("Placed at mile markers: ");
+		for (Integer x : greedyResult) {
+			System.out.print(x + " ");
+		}
+		
 	}
 	
-	private static int[] greedyTowerPlacement(int[] houses) {
+	private static ArrayList<Integer> greedyTowerPlacement(int[] houses) {
 		
+		int[] roadWithHouses = new int[houses[houses.length-1] + 1]; // Create an array which will simulate the road
+		ArrayList<Integer> cellTowers = new ArrayList<Integer>();
+		for (int x: houses) { // Every mile marker with a house will be denoted by a 1
+			roadWithHouses[x] = 1;
+		}
+		
+		Hashtable<Integer,Integer> coverages;
+		do {
+			coverages = new Hashtable<Integer,Integer>();
+			int index = 9;
+			do {
+				int coverage = 0;
+				for(int j = index-9; j < roadWithHouses.length && j <= index+9; j++) {
+					if (roadWithHouses[j] == 1)
+						coverage++;
+				}
+				coverages.put(index, coverage);
+				index++;
+			}while(index < roadWithHouses.length - 10);
+			int mostCoverage = 0;
+			int mileMarker = 0;
+			
+			// Determine which potential cell tower has the most coverage
+			for(Entry<Integer, Integer> entry : coverages.entrySet()) {
+				if(entry.getValue() > mostCoverage) {
+					mostCoverage = entry.getValue();
+					mileMarker = entry.getKey();
+				}
+			}
+			// Build the cell tower with the most coverage, flip the covered houses to 0
+			cellTowers.add(mileMarker);
+			for(int i = mileMarker-9; i < roadWithHouses.length && i <= mileMarker+9; i++) {
+				roadWithHouses[i] = 0;
+			}
+		}while(!coverages.isEmpty());
+		
+		return cellTowers;
 	}
 	
 	private static ArrayList<Integer> bruteForceTowerPlacement(int[] houses) {
